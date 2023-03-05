d1cb2dab749eefd42b390a37fe7dc67a16fa00b2
drew666v <drew.vas@yandex.ru>
2023-03-05 20:09:29 +0800

    make formula shorter

--- js/functions.js
+++ js/functions.js
@@ -2,38 +2,31 @@
 Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true, если строка меньше или равна указанной длине,
 и false, если строка длиннее. */
 
-function verifyLength (string, maxLength) {
-  if (string.length > maxLength) {
-    return false;
-  }
-
-  return true;
-}
+const verifyLength = (string, maxLength) => string.length <= maxLength;
 
 /* Функция для проверки, является ли строка палиндромом.
 Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево. */
 
-function isPalindrome(string) {
+const isPalindrome = (string) => {
   string = string.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
   const reverse = string.split('').reverse().join('');
-  if (string === reverse) {
-    return true;
-  }
-
-  return false;
-}
+  return string === reverse;
+};
 
 /* Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 Если в строке нет ни одной цифры, функция должна вернуть NaN */
 
-function getNumber (string) {
-  if (string.match(/[1-9]/g)) {
-    string = string.replace(/[^0-9]/g, '');
+const getNumber = (string) => {
+  if (typeof string === "number") {
     return string;
   }
 
+  if (string.match(/[1-9]/g)) {
+    return string.replace(/[^0-9]/g, '');
+  }
+
   return NaN;
-}
+};
 
 /* Функция, которая принимает три параметра:
 -исходную строку
@@ -43,6 +36,13 @@ function getNumber (string) {
 Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться.
 Если «добивка» слишком длинная, она обрезается с конца. */
 
-function fillStrLength (originStr, minLength, extraStr) {
-  return originStr.padStart(minLength, extraStr);
-}
+const fillStrLength = (originStr, minLength, extraStr) => originStr.padStart(minLength, extraStr);
+
+const myPadStart = (originStr, minLength, extraStr) => {
+  const actualLength = minLength - originStr.length;
+  if (actualLength <= 0) {
+    return originStr;
+  }
+
+  return extraStr.slice(0, actualLength % extraStr.length) + extraStr.repeat(actualLength / extraStr.length) + originStr;
+};
